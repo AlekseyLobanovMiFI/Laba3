e0d12ea37614df91f12b8bfd472600d6830a5ae6
AlekseyLobanovMiFI <aleks.i.lobanov@gmail.com>
2026-04-26 15:12:53 +0300

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,27 @@
+template<class T> class Stack {
+private:
+    MutableArraySequence<T> data;
+
+public:
+    void Push(const T& item){
+        data.Append(item);
+    }
+
+    T Pop() {
+        if (data.GetLength() == 0){
+            throw std::out_of_range("Empty stack");
+        }
+
+        T val = data.GetLast();
+        data = data.RemoveLast();
+        return val;
+    }
+
+    T Top() const {
+        return data.GetLast();
+    }
+
+    bool IsEmpty() const {
+        return data.GetLength() == 0;
+    }
+};
